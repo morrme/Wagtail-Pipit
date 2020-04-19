7e5dbc6817a5489173b81ddab21cb8185922bf81
Martin Sandström <martin@marteinn.se>
2020-04-19 10:38:13 +0200

    Use checksum for index.html as cache key #168

--- Company-Project/src/pipit/templatetags/extract_elements.py
+++ Company-Project/src/pipit/templatetags/extract_elements.py
@@ -1,6 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-from django.contrib.staticfiles import finders
+from django.contrib.staticfiles import finders, storage
 from django.utils.safestring import mark_safe
 from django.templatetags.static import static
 from bs4 import BeautifulSoup
@@ -9,27 +9,32 @@ from bs4 import BeautifulSoup
 register = template.Library()
 
 
+@register.filter
+def hashed_name(source):
+    if not hasattr(storage.staticfiles_storage, "hashed_name"):
+        return source
+    return storage.staticfiles_storage.hashed_name(source)
+
+
 @register.simple_tag
 def extract_elements(source, element_type, src_prefix="", *args, **kwargs):
-    print(['extract_elements', element_type])
-
     absolute_path = finders.find(source)
 
     with open(absolute_path) as fp:
         soup = BeautifulSoup(fp, "html5lib")
 
-    if element_type == 'stylesheet':
-        tags = soup.find_all(rel='stylesheet')
-    if element_type == 'script':
-        tags = soup.find_all('script')
+    if element_type == "stylesheet":
+        tags = soup.find_all(rel="stylesheet")
+    if element_type == "script":
+        tags = soup.find_all("script")
 
     for tag in tags:
         source_attribute_name = None
 
-        if tag.name == 'script':
-            source_attribute_name = 'src'
-        if tag.name == 'link':
-            source_attribute_name = 'href'
+        if tag.name == "script":
+            source_attribute_name = "src"
+        if tag.name == "link":
+            source_attribute_name = "href"
 
         if source_attribute_name not in tag.attrs:
             continue
