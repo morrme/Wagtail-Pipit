696f88bce0139c3d5e68fc7c006886b9919c613e
Martin Sandström <martin@marteinn.se>
2020-04-28 07:29:56 +0200

    Lets keep serviceWorker support but unregistrated

--- Company-Project/frontend/src/index.js
+++ Company-Project/frontend/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import md5 from 'blueimp-md5';
 import './index.scss';
 import containers from './containers';
-
-//import * as serviceWorker from './serviceWorker';
-
-//ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.unregister();
 
 
 var params = new URLSearchParams(window.location.search)
