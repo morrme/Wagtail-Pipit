fc60dc2d04b54a2ac4635c49952e450309b685dc
Martin Sandström <martin@marteinn.se>
2020-04-30 11:02:57 +0200

    Update default styling to showcase how asset embed works #168

--- Company-Project/frontend/src/components/Hero/Hero.js
+++ Company-Project/frontend/src/components/Hero/Hero.js
@@ -4,7 +4,13 @@ import './Hero.scss';
 
 const Hero = ({ title }) => (
     <div className="Hero">
-        <h1 className="Hero__Title">{title}</h1>
+        <h1 className="Hero__Title">
+            <img
+                src={process.env.PUBLIC_URL + '/assets/img/white_circle.png'}
+                alt="Logo"
+                className="Hero__TitleIcon"
+            />
+        {title}</h1>
     </div>
 );
 
